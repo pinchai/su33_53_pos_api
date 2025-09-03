c02da93a17f89d2e5d7c6e3e8f5ee0c9feb1b935
Pinchai <>
2025-09-03 15:57:10 +0700

    add migration

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+import model
 
 
 @app.route('/')
